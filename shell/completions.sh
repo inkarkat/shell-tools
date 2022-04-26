ff6161272b85ee6e72dc1b7dfc2191bd6cf990fa
Ingo Karkat <swdev@ingo-karkat.de>
2022-04-26 14:12:45 +0200

    completions: Cosmetics: Break long line

--- shell/completions.sh
+++ shell/completions.sh
@@ -1,3 +1,8 @@
 #!/bin/sh source-this-script
 
-completeAsCommand commandlineToShellCommand eachArg eachFile exitsWith onfile processAddedFiles replify reverseFileArgs slowcommand sortedFileArgs tempdir tempfile withAccessibleFile withDir withHome withPath withPid withTempfile
+completeAsCommand commandlineToShellCommand \
+    eachArg eachFile \
+    exitsWith onfile \
+    processAddedFiles replify reverseFileArgs slowcommand sortedFileArgs \
+    tempdir tempfile \
+    withAccessibleFile withDir withHome withPath withPid withTempfile
