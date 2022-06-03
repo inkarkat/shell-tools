0ccc433a64ffd9ff25de71604d649f45c28dd022
Ingo Karkat <swdev@ingo-karkat.de>
2022-06-03 16:58:34 +0200

    aliases: Add alias support for each{Dir,File}

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -3,3 +3,14 @@
 INITIAL_ARGUMENT_COUNT=1 addAliasSupport withPath \
     'C' \
     'clean'
+
+NO_DASHDASH=t UNTIL_DASHDASH=t addAliasSupport eachDir \
+    'l' \
+    'local-dir|with-header|with-prompt|trailing-prompt|no-trailing-prompt|no-output-no-prompt|no-output-no-error|emulate-prompt|no-success-no-count|exit-on-success|exit-on-failure|worst-status' \
+    '' \
+    'initial-status|no-count-on|exit-on|comment|summarize-failed|summarize-failed-separator|between-command|usage-redirect'
+NO_DASHDASH=t UNTIL_DASHDASH=t addAliasSupport eachFile \
+    'l' \
+    'local-dir|with-header|with-prompt|trailing-prompt|no-trailing-prompt|no-output-no-prompt|no-output-no-error|emulate-prompt|no-success-no-count|exit-on-success|exit-on-failure|worst-status' \
+    '' \
+    'initial-status|no-count-on|exit-on|comment|summarize-failed|summarize-failed-separator|between-command|usage-redirect'
