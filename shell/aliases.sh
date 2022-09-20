28cdfcf9293b412082cba6b63daf94cf11a2738a
Ingo Karkat <swdev@ingo-karkat.de>
2022-09-20 08:18:10 +0200

    withAliasedCommand: Command completion, withAliasedCommanda alias

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,9 +1,5 @@
 #!/bin/sh source-this-script
 
-INITIAL_ARGUMENT_COUNT=1 addAliasSupport withPath \
-    'C' \
-    'clean'
-
 NO_DASHDASH=t UNTIL_DASHDASH=t addAliasSupport eachDir \
     'l' \
     'local-dir|with-header|with-prompt|trailing-prompt|no-trailing-prompt|no-output-no-prompt|no-output-no-error|emulate-prompt|no-success-no-count|exit-on-success|exit-on-failure|worst-status' \
@@ -14,3 +10,15 @@ NO_DASHDASH=t UNTIL_DASHDASH=t addAliasSupport eachFile \
     'local-dir|with-header|with-prompt|trailing-prompt|no-trailing-prompt|no-output-no-prompt|no-output-no-error|emulate-prompt|no-success-no-count|exit-on-success|exit-on-failure|worst-status' \
     '' \
     'initial-status|no-count-on|exit-on|comment|summarize-failed|summarize-failed-separator|between-command|usage-redirect'
+
+# Note: Aim for support of NAME=ALIAS-COMMAND (once);
+# --alias ALIAS-COMMAND [...] ; cannot be parsed right now with addAliasSupport.
+INITIAL_ARGUMENT_COUNT=1 addAliasSupport withAliasedCommand \
+    '' \
+    '' \
+    'n' \
+    'name|alias|alias-command'
+
+INITIAL_ARGUMENT_COUNT=1 addAliasSupport withPath \
+    'C' \
+    'clean'
