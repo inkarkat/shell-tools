1b458d936e25ef0121ad9e03e256a7a5cbcda76d
Ingo Karkat <swdev@ingo-karkat.de>
2022-12-04 22:20:37 +0100

    exports: Define commands that complete commands as COMMANDNAME_WRAPPERS for commandName to remove

---
+++ profile/exports.sh
@@ -0,0 +1,6 @@
+IFS='|' optionmunge export COMMANDNAME_WRAPPERS commandlineToShellCommand \
+    eachArg eachDir eachFile \
+    exitsWith onfile \
+    processAddedFiles replify reverseFileArgs slowcommand sortedFileArgs \
+    tempdir tempfile \
+    withAccessibleFile withAliasedCommand withDir withHome withLoggingTo withOutputToSink withPath withPid withTempfile withVisualizedStatus
