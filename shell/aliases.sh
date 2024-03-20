6b77943f62ce0a31742374e36334e6c718cb9525
Ingo Karkat <swdev@ingo-karkat.de>
2024-03-20 19:05:12 +0100

    aliases: Refactoring: Use runWithPrompt --addAliasSupport

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,16 +1,17 @@
 #!/bin/sh source-this-script
 
-NO_DASHDASH=t UNTIL_DASHDASH=t addAliasSupport eachDir \
+NO_DASHDASH=t UNTIL_DASHDASH=t eval "$(runWithPrompt --addAliasSupport eachDir \
     'l' \
-    'local-dir|with-header|with-prompt|trailing-prompt|no-trailing-prompt|no-output-no-prompt|no-output-no-error|emulate-prompt|no-success-no-count|exit-on-success|exit-on-failure|worst-status' \
+    'local-dir' \
     '' \
-    'initial-status|no-count-on|exit-on|comment|summarize-failed|summarize-failed-separator|between-command|usage-redirect'
-NO_DASHDASH=t UNTIL_DASHDASH=t addAliasSupport eachFile \
+    ''
+)"
+NO_DASHDASH=t UNTIL_DASHDASH=t eval "$(runWithPrompt --addAliasSupport eachFile \
     'l' \
-    'local-dir|with-header|with-prompt|trailing-prompt|no-trailing-prompt|no-output-no-prompt|no-output-no-error|emulate-prompt|no-success-no-count|exit-on-success|exit-on-failure|worst-status' \
+    'local-dir' \
     '' \
-    'initial-status|no-count-on|exit-on|comment|summarize-failed|summarize-failed-separator|between-command|usage-redirect'
-
+    ''
+)"
 # Note: Aim for support of NAME=ALIAS-COMMAND (once);
 # --alias ALIAS-COMMAND [...] ; cannot be parsed right now with addAliasSupport.
 INITIAL_ARGUMENT_COUNT=1 addAliasSupport withAliasedCommand \
